3daf2bc11834209ff9859c9ec3d78e668f427aa9
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-17 14:17:10 +0600

    3_making it functional with javascript_part1_s2t6

---
+++ JsCounterApp/index.js
@@ -0,0 +1,13 @@
+const countAddBtn = document.getElementById("add");
+
+countAddBtn.addEventListener("click", () => {
+
+    console.log("Hello from the add button");
+});
+
+/*
+-find the html element
+-tell the element to listen to an event
+-execute logic when event is triggered
+
+*/
